22d643ea38f25057c92e5c83f6b6cc74af59ea1f
WallScripts <wallcejosiel3@gmail.com>
2024-11-09 22:22:14 -0300

    Update WallHub.lua

--- WallHub.lua
+++ WallHub.lua
@@ -493,6 +493,25 @@ Tab:AddToggle({
     Callback = toggleInfiniteJump
 })
 
+Tab:AddButton({
+	Name = "Executar",
+	Callback = function()
+        local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
+
+        workspace.FallenPartsDestroyHeight = -1000
+        local lastCFrame = hrp.CFrame
+
+        hrp.CFrame = CFrame.new(Vector3.new(0, -500, 0))
+
+        wait(0.7)
+
+        hrp.CFrame = lastCFrame
+        workspace.FallenPartsDestroyHeight = -500
+                NotificationOn("Antibang")
+      		print("botão pressionado")
+  	end    
+})
+
 local Tab = Window:MakeTab({
 	Name = "Doors👁",
 	Icon = "rbxassetid://7733799795",
