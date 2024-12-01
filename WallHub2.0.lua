4a5488422ff8b1c82e5a1df9e4b3f561aa4abf37
WallScripts <wallcejosiel3@gmail.com>
2024-12-01 19:16:01 -0300

    Update WallHub2.0.lua

--- WallHub2.0.lua
+++ WallHub2.0.lua
@@ -1,4 +1,4 @@
-local WallHub = loadstring(game:HttpGet(("https://raw.githubusercontent.com/WallScripts/OrionLibraryV3/refs/heads/main/WallHub")))()
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/WallScripts/OrionLibraryV3/refs/heads/main/WallHub")))()
 local Window = OrionLib:MakeWindow({IntroText = "Wall Hub(beta)", IntroIcon = "rbxassetid://8834748103",Name = ("Wall Hub 1.3 | By Wall Scripts".." | ".. identifyexecutor()),IntroToggleIcon = "rbxassetid://7734091286", HidePremium = false, SaveConfig = false, InEnabled = true, ConfigFolder = "Wall Hub"})
 
 Local HomeTab = Window:MakeTab({
